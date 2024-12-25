33b8214f306fdd206fd47d7ef562f6f2bf88b60d
Je-Joestar24 <parrillajejomar@gmail.com>
2024-12-25 22:50:08 +0800

    Template - Navigation template created
    - Navigation:
      - Implemented dynamic loading of navigation links:
        - Navigation links are now dynamically generated to enhance modularity and reusability.

--- js/template/navigations.js
+++ js/template/navigations.js
@@ -6,9 +6,35 @@ export default class extends AbstractTemplate {
     super();
   }
 
+  active = "Home"
+
+  navlinks = [
+    {
+      href: "#/",
+      textContent: "Home",
+    },
+    {
+      href: "#/arcs",
+      textContent: "Arcs",
+    },
+    {
+      href: "#/about",
+      textContent: "About",
+    },
+  ];
+
   async getHtml() {
     return `
-    
+        <ul class="nav__contents">
+          <li class="nav__logo">
+            <a href="#/" aria-label="ArcVoyage Home"> Arc<span>Voyage</span> </a>
+          </li>
+          <div class="nav__links">
+            ${ this.navlinks.map(lnk => `<li>
+              <a href="${lnk.href}" class="nav__link ${lnk.textContent == this.active ? "active" : ""}" aria-label="${lnk.textContent}" data-link
+                >${lnk.textContent}</a>
+            </li>`).join("")}
+          </div>
       `;
   }
 
