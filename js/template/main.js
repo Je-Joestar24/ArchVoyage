6640af28fe68e40a772a5da78e0f608a76cd6a66
Je-Joestar24 <parrillajejomar@gmail.com>
2024-12-25 21:41:00 +0800

    Template - Footer Template Created
    - Footer:
      - Properly created the footer and modularized its designated contents:
        - Footer:
          - Created as a submodule, importing all other contents and calling their [getHtm] methods to generate the contents.
            - Description.
            - Navigation links.
            - Social description.
            - Acknowledgments.
            - Copyright information.
          - Removed hardcoded HTML from [index.html] for:
            - #app__display
            - #footer and its child elements.
    - Main.js:
      - Updated to call the [init] function to load and initialize the footer.

--- js/template/main.js
+++ js/template/main.js
@@ -1,14 +1,17 @@
 import Builder from "./builder.js";
 import AbstractTemplate from "./AbstractTemplate.js";
+import Footer from "./footer.js";
 
 export default class extends AbstractTemplate {
     constructor() {
         super();
         new Builder();
+        this.footer = new Footer();
+        this.init();
     }
 
     async init() {
-        document.getElementById('app__nav').innerHTML = await this.nav.getHtml();
+        //document.getElementById('app__nav').innerHTML = await this.nav.getHtml();
         document.getElementById('footer').innerHTML = await this.footer.getHtml();
     }
 }
